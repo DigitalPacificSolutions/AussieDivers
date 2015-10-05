d1d2d65d2090bb1c6e297cad67dbfe3c1d0b4cbc
QuantumCuriosity <alexskatesutah@yahoo.com>
2015-10-05 09:19:52 -0600

    I'm on Customer now

---
+++ AussieDivers/Controllers/InventoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVCSite.Controllers
+{
+    public class InventoryController : Controller
+    {
+        // GET: Inventory
+        public ActionResult Inventory()
+        {
+            return View();
+        }
+    }
+}
