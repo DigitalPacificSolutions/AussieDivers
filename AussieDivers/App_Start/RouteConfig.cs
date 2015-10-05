8e257993e6b551d4c42e496035128a29cb447d55
Forrest <forrestjtait@hotmail.com>
2015-10-05 15:54:46 -0600

    Fix styles, move hamburger to menu

--- AussieDivers/App_Start/RouteConfig.cs
+++ AussieDivers/App_Start/RouteConfig.cs
@@ -15,8 +15,14 @@ namespace TestMVCSite
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "",
                 defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "ControllerAction",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
