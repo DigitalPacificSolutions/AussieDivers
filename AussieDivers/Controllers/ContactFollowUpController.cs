ce97e90aa62f9572c843d2a8f4c1c48740b9156d
christiaanjohns <christiaanjohns@gmail.com>
2015-10-07 08:01:00 -0600

    Add ContactFollowUp controller and view.

---
+++ AussieDivers/Controllers/ContactFollowUpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVCSite.Controllers
+{
+    public class ContactFollowUpController : Controller
+    {
+        // GET: ContactFollowUp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
