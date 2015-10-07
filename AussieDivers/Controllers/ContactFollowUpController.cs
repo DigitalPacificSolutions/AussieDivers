b4cf6a2d2c205db57f99c80de02e7bc6aab55f51
christiaanjohns <christiaanjohns@gmail.com>
2015-10-07 08:46:45 -0600

    Worked on Contact FollowUp

--- AussieDivers/Controllers/ContactFollowUpController.cs
+++ AussieDivers/Controllers/ContactFollowUpController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AussieDivers.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,12 @@ namespace TestMVCSite.Controllers
 {
     public class ContactFollowUpController : Controller
     {
+        private AussieDiversContext db = new AussieDiversContext();
+
         // GET: ContactFollowUp
         public ActionResult Index()
         {
-            return View();
+            return View(db.People.ToList());
         }
     }
 }
